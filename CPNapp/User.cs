de9414c39f0cec068fef9a94b2f6cea42844d9f6
Miroslaw Puczynski <m_puczynski@ms.wwsi.edu.pl>
2020-12-31 11:54:28 +0100

    add user model

---
+++ CPNapp/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPNapp
+{
+    class User
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Role { get; set; }
+
+    }
+}
